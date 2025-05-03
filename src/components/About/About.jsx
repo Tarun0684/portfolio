bcb8736404bb38910d7699cde4e75b3e21096bbb
tarunkarnwal <karnwaltarunsnm@gmail.com>
2025-05-03 11:51:30 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,12 +13,12 @@ function About() {
       With a strong focus on performance and user experience, I strive to write clean, maintainable code and continuously learn new techniques to stay up-to-date in the ever-evolving world of web development. Whether working on solo projects or collaborating with teams, I bring problem-solving, creativity, and attention to detail to everything I build.
       </p>
       
-      <a
-        href="http://localhost:5173/contact"
+      <Link
+        to="/contact"
         className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 transition duration-300"
       >
         Get in Touch
-      </a>
+      </Link>
     </div>
   )
 }
