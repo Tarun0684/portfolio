3f852069c57b267ccbb3cce604f7a55042a14c61
tarunkarnwal <karnwaltarunsnm@gmail.com>
2026-03-16 22:19:20 +0530

    Change header font size from 2xl to xl

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="bg-gray-800 text-white p-4 w-full fixed top-0 left-0 z-10 shadow-md " >
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Tarun Karnwal</h1>
+        <h1 className="text-xl font-bold">Tarun Karnwal</h1>
         <nav>
           <ul className="flex space-x-4">
             <li>
