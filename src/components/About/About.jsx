6d4b3496b51129eb2888a00517abd8de5ebd3e40
tarunkarnwal <karnwaltarunsnm@gmail.com>
2026-03-17 20:31:17 +0530

    Revise About Me section for clarity and detail
    Updated the About section to enhance clarity and professionalism in the description.

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,14 +5,17 @@ function About() {
   return (
     <div className="min-h-screen bg-white text-gray-800 px-6 py-20" >
       <h2 className="text-4xl font-bold text-gray-800 mb-4">About Me</h2>
-      <p className="text-lg text-gray-600 mb-4">
-     “MBA (Business Analytics & Marketing) Candidate | B.Tech CSE | Aspiring Data Analyst”
+      <p className="text-lg font-bold text-gray-600 mb-4">
+     MBA Candidate (Business Analytics & Marketing) | B.Tech CSE | Aspiring Data Analyst
+        <br/>
+Data Analysis | Excel | SPSS | SQL | Power BI | Problem Solving
 
 
       </p>
       <p className="text-lg text-gray-600 mb-4">
-      I am currently pursuing an MBA specializing in Business Analytics and Marketing and aiming to build my career in Data Analytics and Data Science. With a strong technical background from my B.Tech in Computer Science Engineering, I am passionate about analyzing data, discovering insights, and supporting data-driven business decisions.
-      </p>
+     I am an MBA candidate specializing in Business Analytics and Marketing, with a strong foundation in Computer Science Engineering. I am passionate about data analytics and enjoy transforming raw data into meaningful insights that support data-driven decision-making.
+
+With skills in data analysis, problem-solving, and frontend development, I aim to bridge the gap between technology and business by delivering efficient and insight-driven solutions. I am continuously learning and exploring tools and techniques to enhance my analytical capabilities and contribute effectively in data-focused roles.      </p>
       
       <Link
         to="/contact"
